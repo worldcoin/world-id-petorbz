c3a60eb24056939f5a729106d9afa6a8640cdff7
Konstantin Vyatkin <tino@ottofeller.com>
2022-06-14 12:23:05 +0200

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import { awscdk, javascript } from "projen";
+import { ReactTypeScriptProject, TailwindConfig } from "projen/lib/web";
+
+const project = new awscdk.AwsCdkTypeScriptApp({
+  name: "petzobz-backend",
+  license: "MIT",
+  copyrightOwner: "Humanity Corporation",
+  packageManager: javascript.NodePackageManager.NPM,
+  projenrcTs: true,
+  defaultReleaseBranch: "main",
+  cdkVersion: "2.27.0",
+  constructsVersion: "10.1.39",
+  jest: false,
+  github: false,
+  mergify: false,
+  prettier: true,
+  srcdir: "src",
+  testdir: "src",
+});
+
+const landingPageProject = new ReactTypeScriptProject({
+  parent: project,
+  name: "Landing page frontend",
+  packageName: "landing-page",
+  defaultReleaseBranch: "main",
+  packageManager: javascript.NodePackageManager.NPM,
+  github: false,
+  outdir: path.join(project.srcdir, "landing-page"),
+  typescriptVersion: "^4.7",
+  licensed: false,
+  depsUpgrade: false,
+  readme: { filename: ".gitignore" /* prevent creating README.md */ },
+});
+landingPageProject
+  .tryFindObjectFile("package.json")
+  ?.addOverride("eslintConfig.root", true);
+new TailwindConfig(landingPageProject);
+landingPageProject.addDeps("classnames");
+landingPageProject.addDevDeps("tailwindcss", "prettier-plugin-tailwindcss");
+
+landingPageProject.synth();
+const landingPageDir = path.relative(project.outdir, landingPageProject.outdir);
+for (const tsconfig of [project.tsconfig, project.tsconfigDev]) {
+  tsconfig?.addExclude(landingPageDir);
+}
+
+project.synth();
