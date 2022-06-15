6bad40847b6a76f07cd3d7ced843f1798d8634f7
Artem Rudenko <gvidon@ottofeller.com>
2022-06-15 16:05:59 +0200

    Format the code wld-2980

--- src/svg-transformation/index.ts
+++ src/svg-transformation/index.ts
@@ -5,12 +5,18 @@ export class SvgTransformation extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
-    const lambda = new cdk.aws_lambda_nodejs.NodejsFunction(this, "svg-transformation", {
-      entry: "src/svg-transformation/functions/svg-transformation.ts",
-      handler: "svgTransformation",
-      runtime: cdk.aws_lambda.Runtime.NODEJS_16_X,
-    });
+    const lambda = new cdk.aws_lambda_nodejs.NodejsFunction(
+      this,
+      "svg-transformation",
+      {
+        entry: "src/svg-transformation/functions/svg-transformation.ts",
+        handler: "svgTransformation",
+        runtime: cdk.aws_lambda.Runtime.NODEJS_16_X,
+      }
+    );
 
-    this.exportValue(lambda.functionName, { name: "svg-transformation-function-name" });
+    this.exportValue(lambda.functionName, {
+      name: "svg-transformation-function-name",
+    });
   }
 }
