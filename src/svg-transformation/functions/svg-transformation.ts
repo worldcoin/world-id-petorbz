8fea04ba309db05c4b8e59b21d07447ff6f51c54
Artem Rudenko <gvidon@ottofeller.com>
2022-06-16 11:23:01 +0200

    Add a simple input param to the lambda wld-2980

--- src/svg-transformation/functions/svg-transformation.ts
+++ src/svg-transformation/functions/svg-transformation.ts
@@ -1,3 +1,7 @@
-export const svgTransformation = () => {
-  console.log("Test!");
+import { APIGatewayEvent } from "aws-lambda";
+
+export const svgTransformation = (event: APIGatewayEvent) => {
+  if (event.body) {
+    console.log(JSON.parse(event.body).param);
+  }
 };
