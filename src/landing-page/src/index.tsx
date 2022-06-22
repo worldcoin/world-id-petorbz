13a10c84af242ba0cbe4c4a2a5ac79bfcbd7c154
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-06-22 16:24:41 -0400

    Upgrade to React 18

--- src/landing-page/src/index.tsx
+++ src/landing-page/src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import { App } from "./App";
@@ -16,15 +16,16 @@ const { chains, provider } = configureChains(
 const { connectors } = getDefaultWallets({ appName: "PetOrbz", chains });
 const wagmiClient = createClient({ autoConnect: true, connectors, provider });
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
   <React.StrictMode>
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
         <App />
       </RainbowKitProvider>
     </WagmiConfig>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
