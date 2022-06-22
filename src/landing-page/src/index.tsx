b5be8759a2e2b45532694f397654e9e3cbf9284a
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-06-21 21:31:42 -0400

    rainbowkit

--- src/landing-page/src/index.tsx
+++ src/landing-page/src/index.tsx
@@ -3,10 +3,26 @@ import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import { App } from "./App";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai, chain.polygon],
+  [infuraProvider({ infuraId: process.env.INFURA_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({ appName: "PetOrbz", chains });
+const wagmiClient = createClient({ autoConnect: true, connectors, provider });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>,
   document.getElementById("root")
 );
