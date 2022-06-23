874c6a82f7232e0b137e4e95c0964f73fa8c6f35
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-06-23 11:37:54 -0400

    make WC work

--- src/landing-page/src/index.tsx
+++ src/landing-page/src/index.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable import/first */
+import { Buffer } from "buffer";
+// @ts-ignore
+window.Buffer = Buffer;
 import React from "react";
 import { createRoot } from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
