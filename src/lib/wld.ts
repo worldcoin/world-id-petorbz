9e079db1e2588760d4ba31782032f11c27a4225b
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-08-09 16:54:05 -0700

    add meta-transactions

--- src/lib/wld.ts
+++ src/lib/wld.ts
@@ -1,4 +1,8 @@
-import { solidityPack, solidityKeccak256 } from "ethers/lib/utils";
+import {
+  solidityPack,
+  solidityKeccak256,
+  defaultAbiCoder as abi,
+} from "ethers/lib/utils";
 
 export const encode = (userAddress: string, orbName: string): string => {
   if (!userAddress || !orbName) return "";
@@ -13,4 +17,8 @@ export const encode = (userAddress: string, orbName: string): string => {
   ).toString(16);
 
   return `0x${rawDigest.padStart(64, "0")}`;
+};
+
+export const decode = <T>(type: string, encodedString: string): T => {
+  return abi.decode([type], encodedString)[0];
 };
