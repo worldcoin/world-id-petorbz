491fd4b17111b8c35cdb2b4d1d60d6b9827c478e
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-08-22 14:04:50 -0500

    update

--- src/lib/wld.ts
+++ src/lib/wld.ts
@@ -1,15 +1,23 @@
 import { utils } from "@worldcoin/id";
 import {
   solidityPack,
+  solidityKeccak256,
   defaultAbiCoder as abi,
 } from "ethers/lib/utils";
 
 export const encode = (userAddress: string, orbName: string): string => {
   if (!userAddress || !orbName) return "";
 
-  return utils.worldIDHash(
-    solidityPack(["address", "string"], [userAddress, orbName])
-  ).digest;
+  const rawDigest = (
+    BigInt(
+      solidityKeccak256(
+        ["bytes"],
+        [solidityPack(["address", "string"], [userAddress, orbName])]
+      )
+    ) >> BigInt(8)
+  ).toString(16);
+
+  return `0x${rawDigest.padStart(64, "0")}`;
 };
 
 export const decode = <T>(type: string, encodedString: string): T => {
